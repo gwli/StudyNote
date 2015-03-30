a59dbfb5145055e6f81ff9095d86031ce5a826c8
unknown <gwli@outlook.com>
2015-03-30 23:14:04 +0800

    加入卷积神经网络

--- CNN.py
+++ CNN.py
@@ -8,18 +8,28 @@ rng = numpy.random.RandomState(23455)
 import theano.tensor as T
 import theano
 input = T.tensor4(name='input')
-w_shp = (2, 3, 9, 9)
-w_bound = numpy.sqrt(3 * 9 * 9)
-W = theano.shared( numpy.asarray(
-rng.uniform(
-low=-1.0 / w_bound,
-high=1.0 / w_bound,
-size=w_shp),
-dtype=input.dtype), name ='W')
-b_shp = (2,)
-b = theano.shared(numpy.asarray(
-rng.uniform(low=-.5, high=.5, size=b_shp),
-dtype=input.dtype), name ='b')
+#w_shp = (2, 3, 9, 9)
+#w_bound = numpy.sqrt(3 * 9 * 9)
+#W = theano.shared( numpy.asarray(
+#rng.uniform(
+#low=-1.0 / w_bound,
+#high=1.0 / w_bound,
+#size=w_shp),
+#dtype=input.dtype), name ='W')
+#b_shp = (2,)
+#b = theano.shared(numpy.asarray(rng.uniform(low=-.5, high=.5, size=b_shp),dtype=input.dtype), name ='b')
+
+# <codecell>
+
+# 初始权值
+w_shape = (2,3,9,9)
+w_bound = np.sqrt(3*9*9)
+b_shape = (2,)  
+W = theano.shared(np.asarray(rng.uniform(low = -1.0/w_bound,high = 1.0/w_bound,size = w_shape),dtype= input.dtype),name= 'W')
+b = theano.shared(numpy.asarray(rng.uniform(low=-.5, high=.5, size=b_shape),dtype=input.dtype), name ='b')
+
+# <codecell>
+
 conv_out = conv.conv2d(input, W)
 output = T.nnet.sigmoid(conv_out + b.dimshuffle('x', 0, 'x', 'x'))
 f = theano.function([input], output)
@@ -34,7 +44,7 @@ img = Image.open(open('image_data/image_0001.jpg'))
 
 # <codecell>
 
-img = numpy.asarray(img, dtype='float64') / 256
+img = numpy.asarray(img, dtype='float64') /256
 img_ = img.swapaxes(0, 2).swapaxes(1, 2).reshape(1, 3, width,height)
 filtered_img = f(img_)
 pylab.subplot(1, 3, 1); pylab.axis('off'); pylab.imshow(img)
