61631397cba885b251aeef4be40836e4c5a627bc
unknown <ggzhang@haust.edu.cn>
2015-05-23 17:07:13 +0800

    这个结果是对的，保存下

--- DataAnalysis/BaoKaoFenXi.py
+++ DataAnalysis/BaoKaoFenXi.py
@@ -4,6 +4,7 @@
 # <codecell>
 
 import pandas as pd
+import collections
 
 # <headingcell level=3>
 
@@ -18,25 +19,79 @@ for i in xrange(SheetNum):
 
 # <codecell>
 
-results=[]
 for i in xrange(SheetNum):
     data = BaoKaoDate.parse(BaoKaoDate.sheet_names[1])
 
+# <headingcell level=3>
+
+# 一本分数线控制
+
 # <codecell>
 
-results=[]
-for i in xrange(SheetNum/2):
+result = []
+for i in xrange(SheetNum/4):
     dataL=[]
     dataR =[]
     dataLR=[]
     dataL = BaoKaoDate.parse(BaoKaoDate.sheet_names[i])
     dataR = BaoKaoDate.parse(BaoKaoDate.sheet_names[i+SheetNum/2])
+    #print BaoKaoDate.sheet_names[i]
     dataLR = dataL.append(dataR)
     dataLR.drop_duplicates(cols=u'院校名称', take_last=True, inplace=True)
-    data= pd.DataFrame(dataLR,columns=[u'院校代号',u'院校名称',u'计划人数',u'实际投档人数', u'最低投档分'])
-    #print dataL.shape,dataR.shape,dataLR.shape
+    data= pd.DataFrame(dataLR,columns=[u'院校代号',u'院校名称',u'计划人数',u'实际投档人数', u'超出'])
     data.interpolate(method='bfill')
-    results.append(data)  
+    data2=data[data[u'超出']<=20]
+    data3=data2[data2[u'计划人数']>20]
+    data4=data3[data3[u'实际投档人数']-data3[u'计划人数']<data3[u'计划人数']*0.1]
+    data5= pd.DataFrame(data4,columns=[u'院校代号'])
+    result.append(data5)
+
+# <codecell>
+
+result = pd.concat(result, axis=0) 
+for x,y in collections.Counter(result[u'院校代号']).items():
+    if y>3:
+        print int(x)
+    
+
+# <codecell>
+
+
+# <codecell>
+
+result= pd.DataFrame(data,columns=[u'院校代号'])
+print [x for x,y in collections.Counter(result[u'院校代号']).items() if y>1]
+
+# <codecell>
+
+df
+
+# <codecell>
+
+result.code('utf8')
+
+# <codecell>
+
+lis = ['hoo', 'boo', 'hoo']
+lis.append('boo')
+lis
+
+# <codecell>
+
+print [x for x, y in collections.Counter(lis).items() if y > 1]
+
+# <codecell>
+
+现在要查找重复的。
+
+# <codecell>
+
+import collections
+print [x for x,y in collections.Counter(results).items() if y>1]
+
+# <markdowncell>
+
+# parse 是什么意思？
 
 # <codecell>
 
