3448c0037d7e5e88d868d9d5d7886b3a449490fe
unknown <ggzhang@haust.edu.cn>
2015-05-23 18:31:50 +0800

    现在最终结果是对的，保存一下

--- DataAnalysis/BaoKaoFenXi.py
+++ DataAnalysis/BaoKaoFenXi.py
@@ -8,6 +8,49 @@ import collections
 
 # <headingcell level=3>
 
+# 设定考生考分阀值
+
+# <codecell>
+
+def ChooseSchool(RangeStart,RangeEnd,ChaoChuThreshold,JHRS,Ratio):
+    """
+    设定超出分数线分数
+    设定投档人数下限
+    设定实际投档的人数不能超过计划的百分比
+    RangeStart 表示从那个数据开始
+    RangeEend 从哪个数据结束
+    ChaoChuThreshold 超出分数线多少分，跟一本还是二本有关系
+    JHRS 计划报考人数
+    Ratio 不超过计划的比例
+    """
+    result = []
+    allData=[]
+    for i in xrange(RangeStart,RangeEnd):
+        dataL=[]
+        dataR =[]
+        dataLR=[]
+        dataL = BaoKaoDate.parse(BaoKaoDate.sheet_names[i])
+        dataR = BaoKaoDate.parse(BaoKaoDate.sheet_names[i+SheetNum/2])
+        #print BaoKaoDate.sheet_names[i]
+        dataLR = dataL.append(dataR)
+        dataLR.drop_duplicates(cols=u'院校名称', take_last=True, inplace=True)
+        data= pd.DataFrame(dataLR,columns=[u'院校代号',u'院校名称',u'计划人数',u'实际投档人数', u'超出'])
+        allData.append(data)
+        data.interpolate(method='bfill')
+        data2=data[data[u'超出']<=ChaoChuThreshold]
+        data3=data2[data2[u'计划人数']>JHRS]
+        data4=data3[data3[u'实际投档人数']-data3[u'计划人数']<data3[u'计划人数']*Ratio]
+        data5= pd.DataFrame(data4,columns=[u'院校代号'])
+        result.append(data5)
+    result = pd.concat(result, axis=0) 
+    allData = pd.concat(allData,axis=0)
+    for x,y in collections.Counter(result[u'院校代号']).items():
+        if y>3:
+            print int(x)
+            print allData[allData[u'院校代号']==x]
+
+# <headingcell level=3>
+
 # 列出所有的可以选择的sheet 
 
 # <codecell>
@@ -17,67 +60,19 @@ SheetNum =len(BaoKaoDate.sheet_names)-1
 for i in xrange(SheetNum):
     print BaoKaoDate.sheet_names[i]
 
-# <codecell>
-
-for i in xrange(SheetNum):
-    data = BaoKaoDate.parse(BaoKaoDate.sheet_names[1])
-
 # <headingcell level=3>
 
-# 一本分数线控制
+# 一本院校分析
 
 # <codecell>
 
-result = []
-allData=[]
-for i in xrange(SheetNum/4):
-    dataL=[]
-    dataR =[]
-    dataLR=[]
-    dataL = BaoKaoDate.parse(BaoKaoDate.sheet_names[i])
-    dataR = BaoKaoDate.parse(BaoKaoDate.sheet_names[i+SheetNum/2])
-    #print BaoKaoDate.sheet_names[i]
-    dataLR = dataL.append(dataR)
-    dataLR.drop_duplicates(cols=u'院校名称', take_last=True, inplace=True)
-    data= pd.DataFrame(dataLR,columns=[u'院校代号',u'院校名称',u'计划人数',u'实际投档人数', u'超出'])
-    allData.append(data)
-    data.interpolate(method='bfill')
-    data2=data[data[u'超出']<=20]
-    data3=data2[data2[u'计划人数']>20]
-    data4=data3[data3[u'实际投档人数']-data3[u'计划人数']<data3[u'计划人数']*0.1]
-    data5= pd.DataFrame(data4,columns=[u'院校代号'])
-    result.append(data5)
+ChooseSchool(0,SheetNum/4,20,20,0.1)
+
+# <headingcell level=3>
+
+# 二本院校分析
 
 # <codecell>
 
-result = pd.concat(result, axis=0) 
-allData = pd.concat(allData,axis=0)
-for x,y in collections.Counter(result[u'院校代号']).items():
-    if y>3:
-        print int(x)
-        print allData[allData[u'院校代号']==x]
-
-# <markdowncell>
-
-# parse 是什么意思？
-
-# <codecell>
-
-这里分数线还没有细分到
-
-# <headingcell level=3>
-
-# 设定搜索一本二本范围
-
-# <headingcell level=3>
-
-# 设定阀值，删除数据（可选）
-
-# <headingcell level=3>
-
-# 设定考生考分阀值
-
-# <headingcell level=3>
-
-# 投档的人数不能超过技术的20%
+ChooseSchool(SheetNum/4,SheetNum/2,20,20,0.1)
 
