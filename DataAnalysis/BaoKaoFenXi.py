2a9ba5d24c2d859a2218be65b686ec17b7fa643e
unknown <ggzhang@haust.edu.cn>
2015-05-23 17:49:12 +0800

    现在一本是对的，保存

--- DataAnalysis/BaoKaoFenXi.py
+++ DataAnalysis/BaoKaoFenXi.py
@@ -29,6 +29,7 @@ for i in xrange(SheetNum):
 # <codecell>
 
 result = []
+allData=[]
 for i in xrange(SheetNum/4):
     dataL=[]
     dataR =[]
@@ -39,6 +40,7 @@ for i in xrange(SheetNum/4):
     dataLR = dataL.append(dataR)
     dataLR.drop_duplicates(cols=u'院校名称', take_last=True, inplace=True)
     data= pd.DataFrame(dataLR,columns=[u'院校代号',u'院校名称',u'计划人数',u'实际投档人数', u'超出'])
+    allData.append(data)
     data.interpolate(method='bfill')
     data2=data[data[u'超出']<=20]
     data3=data2[data2[u'计划人数']>20]
@@ -49,61 +51,15 @@ for i in xrange(SheetNum/4):
 # <codecell>
 
 result = pd.concat(result, axis=0) 
+allData = pd.concat(allData,axis=0)
 for x,y in collections.Counter(result[u'院校代号']).items():
     if y>3:
         print int(x)
-    
-
-# <codecell>
-
-
-# <codecell>
-
-result= pd.DataFrame(data,columns=[u'院校代号'])
-print [x for x,y in collections.Counter(result[u'院校代号']).items() if y>1]
-
-# <codecell>
-
-df
-
-# <codecell>
-
-result.code('utf8')
-
-# <codecell>
-
-lis = ['hoo', 'boo', 'hoo']
-lis.append('boo')
-lis
-
-# <codecell>
-
-print [x for x, y in collections.Counter(lis).items() if y > 1]
-
-# <codecell>
-
-现在要查找重复的。
-
-# <codecell>
-
-import collections
-print [x for x,y in collections.Counter(results).items() if y>1]
+        print allData[allData[u'院校代号']==x]
 
 # <markdowncell>
 
 # parse 是什么意思？
-
-# <codecell>
-
-print BaoKaoDate.sheet_names[i],data
-
-# <codecell>
-
-    data1=data[data[u'最低投档分']<=567]
-    data1=data[data[u'最低投档分']<=567]
-    data3=data2[data2[u'计划人数']>20]
-    data4=data3[data3[u'实际投档人数']-data3[u'计划人数']<data3[u'计划人数']*0.1]
-    results.append(data4)  
 
 # <codecell>
 
@@ -113,56 +69,15 @@ print BaoKaoDate.sheet_names[i],data
 
 # 设定搜索一本二本范围
 
-# <codecell>
-
-results=[]
-for i in xrange(SheetNum/2):
-    data = BaoKaoDate.parse(BaoKaoDate.sheet_names[i])
-    data1 =data.drop([1],axis=0)
-    data2=data1[data1['最低投档分']<=567]
-    data3=data2[data2[u'计划']>20]
-    data4=data3[data3['Unnamed: 3']-data3['Unnamed: 2']<data3['Unnamed: 2']*0.1]
-    results.append(data4)  
-data4.shape
-#print results
-#pd.DataFrame(np.asarray(results))
-
 # <headingcell level=3>
 
 # 设定阀值，删除数据（可选）
-
-# <codecell>
-
-data.columns
-
-# <codecell>
-
-data1 =data.drop([1],axis=0)
 
 # <headingcell level=3>
 
 # 设定考生考分阀值
 
-# <codecell>
-
-data2.shape
-
-# <codecell>
-
-data3.shape
-
 # <headingcell level=3>
 
 # 投档的人数不能超过技术的20%
-
-# <codecell>
-
-data4.shape
-
-# <codecell>
-
-data4
-
-# <codecell>
-
 
