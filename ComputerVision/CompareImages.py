c693473387a56f59ff4704a30811a01edec54e6f
unknown <ggzhang@haust.edu.cn>
2015-09-06 10:15:37 +0800

    加入两个图片比对功能

--- ComputerVision/CompareImages.py
+++ ComputerVision/CompareImages.py
@@ -18,23 +18,53 @@ AfterProcessing = glob('AP/*.jpg')
 
 # <codecell>
 
+a = np.asarray([0,1,2,3,4])
+
+# <codecell>
+
+a.shape
+
+# <codecell>
+
 a=10
 b=120
 c =10
 d=80
 NN = (b-a)*(d-c)
-for elem in files[1:10]:
-    (filename,ext) = os.path.splitext(elem)
+for elem in BeforeProcessing:
+    (path,filename) = os.path.split(elem)
+    (KaoShengHao,ext) = os.path.splitext(filename)
     BeforeProcessingImage= cv2.imread(elem)
     BeforeProcessingImage.resize((126,126))
-    KaoShengHao =filename[3:18]
     KaoShengHaoBiao = BiDuiBiao['KaoShengHao1']
-    index = KaoShengHaoBiao[int(KaoShengHao)==KaoShengHaoBiao].index[0]
+    index = KaoShengHaoBiao[KaoShengHaoBiao==int(KaoShengHao)]
+    value = index.values
     if value!=[]:
+        print 1
+
+# <codecell>
+
+if value!=[]:
+    print 1
+
+# <codecell>
+
+  if value!=[]:
         AfterProcessingImage= cv2.imread('AP/'+str(XueHao[0])+'.jpg')
         AfterProcessingImage.resize((126,126))
         error=(BeforeProcessingImage[a:b,c:d]-AfterProcessingImage[a:b,c:d])/NN
- 
+
+# <codecell>
+
+KaoShengHaoBiao
+
+# <codecell>
+
+int(KaoShengHao)
+
+# <codecell>
+
+int(KaoShengHao)
 
 # <codecell>
 
